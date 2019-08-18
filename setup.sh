0abe0df97503f611aaa1de8e62660fc6049f1aad
Raj Singh <rajtpd@gmail.com>
2019-08-18 09:23:34 -0700

    changes

--- setup.sh
+++ setup.sh
@@ -1,38 +1,54 @@
 #!/bin/sh
 
-echo "Installing zip and unzip packages...."
+echo -e "\e[92m Starting Setup...\e[0m"
+echo
+echo
+echo -e "\e[92m Installing zip and unzip packages...\e[0m"
+echo
+echo
 sudo apt-get update
 sudo apt-get install zip unzip -y
-echo "Done! Installing zip and unzip packages...."
 echo
 echo
-echo "Installing ansible package...."
+echo -e "\e[92m Done! Installing zip and unzip packages...\e[0m"
+echo
+echo
+echo -e "\e[92m Installing ansible package....\e[0m"
+echo
+echo
 sudo apt update
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
-echo "Done! Installing ansible...."
 echo
 echo
-echo "Encrypting vault"
+echo -e "\e[92m Done! Installing ansible...\e[0m"
+echo
+echo
+echo -e "\e[92m Encrypting vault\e[0m"
+echo
+echo
 ansible-vault encrypt ~/telemetry_workshop/environment/ansible/secrets.yml
-echo "Done encrypting"
 echo
 echo
-echo "Installing Influxdb....."
+echo -e "\e[92m Done encrypting\e[0m"
+echo
+echo
+echo -e "\e[92mInstalling Influxdb.....\e[0m"
 echo
 echo
 wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
 source /etc/lsb-release
 echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
-
+echo
 sudo apt-get update && sudo apt-get install -y influxdb
 sudo systemctl unmask influxdb.service
-sudo systemctl start influxdb
-echo "Done installing Influxdb"
 echo
 echo
-echo "Installing grafana...."
+echo -e "\e[92m Done installing Influxdb\e[0m"
+echo
+echo
+echo -e "\e[92m Installing grafana....\e[0m"
 echo
 echo
 sudo apt-get install -y software-properties-common
@@ -40,7 +56,14 @@ sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y grafana
-echo "Done installing Grafana"
 echo
 echo
+echo -e "\e[92m Done installing Grafana\e[0m"
+echo
+echo
+mv * ../
+cd ..
+rm -rf ./telemetry_workshop
+echo
+echo -e "\e[92mDone with Setup!\e[0m"
 echo
